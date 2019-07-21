8178b63b2bd00c00f10f3da8987633b7b69060c9
Filipe Tagliacozzi <filipednb@gmail.com>
2019-07-21 06:38:15 -0300

    corrigindo porta e properties

---
+++ src/main/java/com/microservice/backend/ApiController.java
@@ -0,0 +1,21 @@
+package com.microservice.backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String helloFromBackend() throws InterruptedException{
+
+        Thread.sleep(100 + new Random().nextInt(500));
+
+        return "Olá mundo";
+    }
+
+
+}
